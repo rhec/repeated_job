761d90a1156ad815c8aac20f54f8e3174b781863
Robert Eanes <github@robsinbox.com>
2010-08-20 12:02:57 -0500

    Make the task to run configurable and make jobs configurable per instance

--- lib/repeated_job.rb
+++ lib/repeated_job.rb
@@ -4,20 +4,21 @@ require "rake"
 module Repeated
   class Job
 
-    attr_reader :interval, :priority
+    attr_reader :interval, :priority, :task
 
-    def initialize
-      @interval = (ENV["REPEATED_JOB_INTERVAL"] || 5).to_i   # minutes
-      @priority = (ENV["REPEATED_JOB_PRIORITY"] || 0).to_i
+    def initialize options={}
+      @interval = (options[:interval] || ENV["REPEATED_JOB_INTERVAL"] || 5).to_i   # minutes
+      @priority = (options[:priority] || ENV["REPEATED_JOB_PRIORITY"] || 0).to_i
+      @task = (options[:task] || ENV["REPEATED_JOB_TASK"] || "cron")
     end
 
     def perform
       schedule_next
-      Rake::Task["cron"].execute
+      Rake::Task[@task].execute
     end
 
     def schedule_next
-      Delayed::Job.delete_all "handler like '%Repeated::Job%'"
+      Delayed::Job.delete_all "handler like '%Repeated::Job%task: #{@task}\n%'"
       Delayed::Job.enqueue self, priority, interval.minutes.from_now.getutc
     end
 
--- spec/repeated_job_spec.rb
+++ spec/repeated_job_spec.rb
@@ -8,10 +8,10 @@ describe "Repeated::Job" do
       ENV["REPEATED_JOB_INTERVAL"] = "10"
       Repeated::Job.new.interval.should == 10
     end
-
-    it "has a configurable priority" do
-      ENV["REPEATED_JOB_INTERVAL"] = "1"
-      Repeated::Job.new.interval.should == 1
+    
+    it "allows overriding the interval when initialized" do
+      ENV["REPEATED_JOB_INTERVAL"] = "10"
+      Repeated::Job.new(:interval => 15).interval.should == 15
     end
 
     it "has a default interval of 5 minutes" do
@@ -19,11 +19,35 @@ describe "Repeated::Job" do
       Repeated::Job.new.interval.should == 5
     end
 
+    it "has a configurable priority" do
+      ENV["REPEATED_JOB_PRIORITY"] = "1"
+      Repeated::Job.new.priority.should == 1
+    end
+
+    it "allows overriding the priority when initialized" do
+      ENV["REPEATED_JOB_PRIORITY"] = "1"
+      Repeated::Job.new(:priority => 2).priority.should == 2
+    end
+
     it "has a default priority of 0" do
       ENV.delete("REPEATED_JOB_PRIORITY")
       Repeated::Job.new.priority.should == 0
     end
+    
+    it "has a configurable rake task" do
+      ENV["REPEATED_JOB_TASK"] = "foo"
+      Repeated::Job.new.task.should == "foo"
+    end
 
+    it "allows overriding the rake task when initialized" do
+      ENV["REPEATED_JOB_TASK"] = "foo"
+      Repeated::Job.new(:task => "baz").task.should == "baz"
+    end
+    
+    it "has a default task of 'cron'" do
+      ENV.delete("REPEATED_JOB_TASK")
+      Repeated::Job.new.task.should == "cron"
+    end
   end
 
   describe "scheduling" do
@@ -35,7 +59,7 @@ describe "Repeated::Job" do
     it "knows how to schedule itself" do
       ENV["REPEATED_JOB_INTERVAL"] = "5"
 
-      Delayed::Job.should_receive(:delete_all).with(/Repeated::Job/)
+      Delayed::Job.should_receive(:delete_all).with(/Repeated::Job%task: cron\n%/)
 
       Delayed::Job.should_receive(:enqueue) do |object, priority, scheduled|
         object.should   == @repeated
@@ -56,6 +80,20 @@ describe "Repeated::Job" do
       @was_run.should be_true
     end
 
+  end
+  
+  describe "performing" do
+    it "should run the specified task" do
+      @foo_was_run = false
+      @cron_was_run = false
+      task(:foo) {@foo_was_run = true}
+      task(:cron) {@cron_was_run = true}
+      repeated = Repeated::Job.new(:task => "foo")
+      repeated.stub!(:schedule_next)
+      repeated.perform
+      @foo_was_run.should be_true
+      @cron_was_run.should be_false
+    end
   end
 
 end
